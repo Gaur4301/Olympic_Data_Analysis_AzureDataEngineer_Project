41479ba112f81e03cef89d6da12161b06e6f0e6b
Gaurav <g.gauravjoshi2017@gmail.com>
2025-06-21 23:11:35 +0530

    new

--- Databricks/Data-Transform.py
+++ Databricks/Data-Transform.py
@@ -1,3 +1,22 @@
+secret_value = dbutils.secrets.get(scope="kv-integration-scope", key="dev-secret")
+# print(secret_value)
+url=f"https://login.microsoftonline.com/{secret_value}/oauth2/token"
+
+configs = {"fs.azure.account.auth.type": "OAuth",
+"fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+"fs.azure.account.oauth2.client.id": "5e7778f5-768d-4432-a4d0-ed6dcfd44496",
+"fs.azure.account.oauth2.client.secret": 'uM-8Q~WCdSItjOBBgUiqf1ewfy_34sBi9Zd.8bSO',
+"fs.azure.account.oauth2.client.endpoint": url}
+
+
+dbutils.fs.mount(
+source = "abfss://raw-data@azurepracticestoragenew.dfs.core.windows.net", # contrainer@storageacc
+mount_point = "/mnt/tokyoolymicdata",
+extra_configs = configs)
+
+%fs
+ls "/mnt/tokyoolymicdata"
+
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 spark=SparkSession.builder.appName('TokyoOlympic').getOrCreate()
